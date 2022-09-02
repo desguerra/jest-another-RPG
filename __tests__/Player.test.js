81cfb12c23cc0d2aab25f2ca9d10b19b1b2fb1a6
desguerra <ddesguerra@ucdavis.edu>
2022-09-02 15:53:57 -0700

    add Player constructor and test with mock data and prototype

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,9 +1,13 @@
 const Player = require('../lib/Player');
 
+// require() line imports the Potion() constructor into the test, 
+// establishing Potion as a usable variable (otherwise new Potion() would throw an error)
 const Potion = require('../lib/Potion');
+// jest.mock() mocks/replaces the constructor's implementation with our faked data
 jest.mock('../lib/Potion');
 
-
+// console log a mocked potion
+// console.log(new Potion());
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -16,4 +20,23 @@ test('creates a player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+});
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+  
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+  
+    expect(player.getInventory()).toEqual(expect.any(Array));
+  
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
 });
